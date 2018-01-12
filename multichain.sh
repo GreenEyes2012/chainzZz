bfead8fc5703dc5b52cbbe21880f54fb9cac215c
unibitlabs <31015318+unibitlabs@users.noreply.github.com>
2018-01-12 12:59:32 -0600

    Update multichain.sh

--- multichain.sh
+++ multichain.sh
@@ -225,16 +225,16 @@ sudo sed -ie 's/MANAGER_ADDRESS =.*;/MANAGER_ADDRESS = "'$addr'";/g' /var/www/ht
 
 
 ###
-## INSTALLING & CONFIGURING YOBIAPPS
+## INSTALLING & CONFIGURING APPS
 ###
-git clone https://github.com/Primechain/yobiapps.git
+git clone https://github.com/unibitlabs/apps.git
 
-# Configuring Yobiapps
-sudo sed -ie 's/$CHAIN_NAME =.*;/$CHAIN_NAME = "'$chainname'";/g' /var/www/html/yobiapps/config.php
-sudo sed -ie 's/RPC_USER =.*;/RPC_USER = "'$rpcuser'";/g' /var/www/html/yobiapps/config.php
-sudo sed -ie 's/RPC_PASSWORD =.*;/RPC_PASSWORD = "'$rpcpassword'";/g' /var/www/html/yobiapps/config.php
-sudo sed -ie 's/RPC_PORT =.*;/RPC_PORT = "'$rpcport'";/g' /var/www/html/yobiapps/config.php
-sudo sed -ie 's/MANAGER_ADDRESS =.*;/MANAGER_ADDRESS = "'$addr'";/g' /var/www/html/yobiapps/config.php
+# Configuring Apps
+sudo sed -ie 's/$CHAIN_NAME =.*;/$CHAIN_NAME = "'$chainname'";/g' /var/www/html/apps/config.php
+sudo sed -ie 's/RPC_USER =.*;/RPC_USER = "'$rpcuser'";/g' /var/www/html/apps/config.php
+sudo sed -ie 's/RPC_PASSWORD =.*;/RPC_PASSWORD = "'$rpcpassword'";/g' /var/www/html/apps/config.php
+sudo sed -ie 's/RPC_PORT =.*;/RPC_PORT = "'$rpcport'";/g' /var/www/html/apps/config.php
+sudo sed -ie 's/MANAGER_ADDRESS =.*;/MANAGER_ADDRESS = "'$addr'";/g' /var/www/html/apps/config.php
 
 
 ###
